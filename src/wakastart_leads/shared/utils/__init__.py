e35c87d767652b5b59e9d2610c4c3299159a190a
theo <theo@wakasign.com>
2026-02-05 10:36:35 +0100

    feat: export parallel_runner functions in utils __init__

--- src/wakastart_leads/shared/utils/__init__.py
+++ src/wakastart_leads/shared/utils/__init__.py
@@ -17,28 +17,42 @@ from .constants import (
 )
 from .csv_utils import clean_markdown_artifacts, load_existing_csv, post_process_csv
 from .log_rotation import cleanup_old_logs, get_log_retention_days
+from .parallel_runner import (
+    CSV_HEADER,
+    RunStatus,
+    UrlResult,
+    merge_results_to_csv,
+    run_parallel,
+    run_single_url,
+)
 from .url_utils import ensure_https, load_urls, normalize_url
 
 __all__ = [
     "ANALYSIS_DIR",
     "ANALYSIS_INPUT",
     "ANALYSIS_OUTPUT",
+    "CSV_HEADER",
     "DEFAULT_BATCH_SIZE",
     "ENRICHMENT_DIR",
     "ENRICHMENT_INPUT",
     "ENRICHMENT_OUTPUT",
     "EXPECTED_COLUMNS",
     "PACKAGE_ROOT",
+    "RunStatus",
     "SEARCH_DIR",
     "SEARCH_INPUT",
     "SEARCH_OUTPUT",
     "URL_COLUMN_INDEX",
+    "UrlResult",
     "clean_markdown_artifacts",
     "cleanup_old_logs",
     "ensure_https",
     "get_log_retention_days",
     "load_existing_csv",
     "load_urls",
+    "merge_results_to_csv",
     "normalize_url",
     "post_process_csv",
+    "run_parallel",
+    "run_single_url",
 ]
